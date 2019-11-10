f7b6d5b1d02dd39fdffb27fab2ad77e26906f673
Radish Logic <radishlogic01@gmail.com>
2019-11-11 00:15:42 +0800

    Add files via upload

---
+++ S3 Website Corrected/js/cognito-auth.js
@@ -0,0 +1,48 @@
+/*global RadishLogicApp _config AmazonCognitoIdentity AWSCognito*/
+
+var RadishLogicApp = window.RadishLogicApp || {};
+
+(function scopeWrapper($) {
+
+    var poolData = {
+        UserPoolId: _config.cognito.userPoolId,
+        ClientId: _config.cognito.userPoolClientId,
+    };
+
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    
+    RadishLogicApp.login = function () {
+        var username = $('#inputUsername').val();
+        var password = $('#inputPassword').val();
+
+        var authenticationData = {
+            Username: username,
+            Password: password,
+        };
+
+        var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+
+        var userData = {
+            Username: username,
+            Pool: userPool,
+        };
+
+        var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: function(result) {
+                window.location = '/signedin.html';
+            },
+            onFailure: function(err) {
+                console.log(err);
+                alert(err.message || JSON.stringify(err));
+            }
+        });
+    }
+
+    $('#signin').click(function(event) {
+        event.preventDefault(); //Overriding the default refresh behavior
+        RadishLogicApp.login();
+    });
+
+}(jQuery));
---
+++ S3 Website Error/js/config.js
@@ -0,0 +1,6 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-1_JjUTJJtde',
+        userPoolClientId: '6qct85jn4hdtdjqn7qu8fhd8d6',
+    }
+};
